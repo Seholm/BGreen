df2e563eb8a1cde20e0290b6a4f62e0e48e9c772
Filip Larsson <filip.larsson.vbg@gmail.com>
2015-09-17 14:21:54 +0200

    Added comments to code in calculate class and interface

--- BGreen/app/src/main/java/com/bgreen/filips/bgreen/ICalculateTravelInfo.java
+++ BGreen/app/src/main/java/com/bgreen/filips/bgreen/ICalculateTravelInfo.java
@@ -5,8 +5,9 @@ package com.bgreen.filips.bgreen;
  */
 public interface ICalculateTravelInfo {
 
-
+    //Method for returning a total distance
     int getFinalResult();
+    //Method which Service class calls to give info for calculating the distance
     void main(boolean lastStop, String nextStopTest, String route);
 
 }
