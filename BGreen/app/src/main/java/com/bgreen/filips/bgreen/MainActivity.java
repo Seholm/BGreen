e36b98716df2eb69b985d6825eb90b937b181ee5
Filip Larsson <filip.larsson.vbg@gmail.com>
2015-09-24 12:48:31 +0200

    Merge branch 'master' into TestWifiConnectivity

--- BGreen/app/src/main/java/com/bgreen/filips/bgreen/MainActivity.java
+++ BGreen/app/src/main/java/com/bgreen/filips/bgreen/MainActivity.java
@@ -1,29 +1,72 @@
 package com.bgreen.filips.bgreen;
 
+import android.content.Intent;
+import android.content.IntentSender;
+import android.support.v7.app.AppCompatActivity;
+
 import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.net.wifi.WifiManager;
 import android.os.SystemClock;
-import android.support.v7.app.ActionBarActivity;
+
+import android.content.IntentSender;
+import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
 import android.util.Base64;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.Scopes;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.Scope;
+import com.google.android.gms.plus.Plus;
+import com.google.android.gms.plus.model.people.Person;
+import com.parse.Parse;
+
+
+public class MainActivity extends AppCompatActivity implements
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        View.OnClickListener {
+
+<<<<<<< HEAD
 import java.io.UnsupportedEncodingException;
 
+=======
+    private final String PARSE_CLIENT_KEY = "0qM0pkPsSmWoEuhqbN4iKHbbSfmgXwLwEJy7ZUHV";
+    private final String PARSE_APPLICATION_ID = "Wi3ExMtOI5koRFc29GiaE3C4qmukjPokmETpcPQA";
+    
+    /* Request code used to invoke sign in user interactions. */
+    private static final int RC_SIGN_IN = 0;
+>>>>>>> master
 
-public class MainActivity extends ActionBarActivity {
+    /* Client used to interact with Google APIs. */
+    private GoogleApiClient mGoogleApiClient;
+
+    /* Is there a ConnectionResult resolution in progress? */
+    private boolean mIsResolving = false;
+
+    /* Should we automatically resolve ConnectionResults when possible? */
+    private boolean mShouldResolve = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+<<<<<<< HEAD
 
         tempFilipMethod();
+=======
+        // Enable Local Datastore, and Parse DB services.
+        Parse.enableLocalDatastore(this);
+        Parse.initialize(this, PARSE_APPLICATION_ID, PARSE_CLIENT_KEY);
+>>>>>>> master
 
         //sets an alarm with 1 minute interval to run the snipplte code in MinuteReciever
         AlarmManager alarmManager=(AlarmManager) getSystemService(ALARM_SERVICE);
@@ -31,6 +74,7 @@ public class MainActivity extends ActionBarActivity {
         PendingIntent pendingIntent = PendingIntent.getBroadcast(MainActivity.this, 0, intent, 0);
         alarmManager.setRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP, SystemClock.elapsedRealtime(),1*60*1000, pendingIntent);
 
+<<<<<<< HEAD
                 // String to be encoded with Base64
                 String text = "grp20:yxY7OD4aQ2";
 // Sending side
@@ -55,7 +99,20 @@ public class MainActivity extends ActionBarActivity {
 
             }
 
+=======
+        // Build GoogleApiClient to request access to the basic user profile and email
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(Plus.API)
+                .addScope(new Scope(Scopes.PROFILE))
+                .addScope(new Scope(Scopes.EMAIL))
+                .build();
+>>>>>>> master
 
+        //OnClicklistner for the signInButton
+        findViewById(R.id.sign_in_button).setOnClickListener(this);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -79,8 +136,123 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+<<<<<<< HEAD
     public void tempFilipMethod(){
         new RetrieveBusData().execute();
 
+=======
+
+    @Override
+    protected void onStart(){
+        // Connect GoogleApiClient
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        // Disconnect GoogleApiClient
+        super.onStop();
+        mGoogleApiClient.disconnect();
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        // onConnected indicates that an account was selected on the device, that the selected
+        // account has granted any requested permissions to our app and that we were able to
+        // establish a service connection to Google Play services.
+        mShouldResolve = false;
+
+        // Show the signed-in UI
+        getProfileInformation();
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.sign_in_button) {
+            onSignInClicked();
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            // If the error resolution was not successful we should not resolve further.
+            if (resultCode != RESULT_OK) {
+                mShouldResolve = false;
+            }
+
+            mIsResolving = false;
+            mGoogleApiClient.connect();
+        }
+    }
+
+    private void onSignInClicked() {
+        // User clicked the sign-in button, so begin the sign-in process and automatically
+        // attempt to resolve any errors that occur.
+        mShouldResolve = true;
+        mGoogleApiClient.connect();
+
+        // Show a message to the user that we are signing in.
+        System.out.println("*******SIGNING IN*********");
+        //TODO;Toast to show user that we are signing in
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        // Could not connect to Google Play Services.  The user needs to select an account,
+        // grant permissions or resolve an error in order to sign in. Refer to the javadoc for
+        // ConnectionResult to see possible error codes.
+
+        if (!mIsResolving && mShouldResolve) {
+            if (connectionResult.hasResolution()) {
+                try {
+                    connectionResult.startResolutionForResult(this, RC_SIGN_IN);
+                    mIsResolving = true;
+                } catch (IntentSender.SendIntentException e) {
+                    mIsResolving = false;
+                    mGoogleApiClient.connect();
+                }
+            } else {
+                // Could not resolve the connection result, show the user an
+                // error dialog.
+                //TODO;showErrorDialog(connectionResult);
+            }
+        } else {
+            //TODO;Show the signed-out UI
+        }
+    }
+
+    //TODO;SPARA PROFILEN I EN EGEN KLASS??
+
+    //Gets the profileIno and put it on the TabActivity
+    private void getProfileInformation(){
+        if (Plus.PeopleApi.getCurrentPerson(mGoogleApiClient) != null) {
+            Person currentPerson = Plus.PeopleApi.getCurrentPerson(mGoogleApiClient);
+
+            String personName = currentPerson.getDisplayName();
+            String personPhotoUrl = currentPerson.getImage().getUrl();
+            String personGooglePlusId = currentPerson.getId();
+            String email = Plus.AccountApi.getAccountName(mGoogleApiClient);
+
+            Intent tabActivityIntent = new Intent(this, TabActivity.class);
+
+            tabActivityIntent.putExtra("PROFILE_NAME", personName);
+            tabActivityIntent.putExtra("PROFILE_MAIL", email);
+            tabActivityIntent.putExtra("PROFILE_IMAGE", personPhotoUrl);
+            tabActivityIntent.putExtra("PROFILE_ID", personGooglePlusId);
+            startActivity(tabActivityIntent);
+        }else{
+
+            System.out.println("****CURRENT PERSON IS NULL*****");
+        }
+>>>>>>> master
     }
 }
+
---
+++ BGreen/app/src/main/java/com/bgreen/filips/bgreen/ProfileFragment.java
@@ -0,0 +1,70 @@
+package com.bgreen.filips.bgreen;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+
+public class ProfileFragment extends Fragment{
+
+    private String profileName;
+    private String profileEmail;
+    private String profileImgURL;
+    private String profileID;
+
+    private String profileRanking = "22";
+    private String profileDistance = "1056";
+
+    private CircleImageView circleImageView;
+    private TextView profileRankingAndDistance;
+
+    public ProfileFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View myInflatedView = inflater.inflate(R.layout.fragment_profile, container,false);
+
+        Bundle bundle = this.getArguments();
+        this.profileName = bundle.getString("PROFILE_NAME_TO_FRAGMENT");
+        this.profileEmail = bundle.getString("PROFILE_EMAIL_TO_FRAGMENT");
+        this.profileImgURL = bundle.getString("PROFILE_IMG_URL_TO_FRAGMENT");
+        this.profileID = bundle.getString("PROFILE_ID_TO_FRAGMENT");
+
+        profileRankingAndDistance = (TextView) myInflatedView.findViewById(R.id.profile_Ranking_Distance_TextView);
+
+        profileRankingAndDistance.setText("#" + profileRanking + "   Distance: "+ profileDistance + "m");
+
+        circleImageView = (CircleImageView) myInflatedView.findViewById(R.id.profile_image);
+
+        //Picasso library loads the image URL and put it into circleImageView
+        Picasso.with(this.getActivity())
+                .load(changeSizeOnURLImage(profileImgURL))
+                .into(circleImageView);
+
+        TextView nameTextView = (TextView) myInflatedView.findViewById(R.id.profile_textView);
+        nameTextView.setText(profileName);
+
+        return myInflatedView;
+    }
+
+    //Change the default size of the image to new size with 150 x 150 px
+    private String changeSizeOnURLImage(String s){
+        String temp = s.substring(0,s.length()-2);
+        temp = temp + "150";
+        return temp;
+    }
+}
