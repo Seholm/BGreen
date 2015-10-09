41508d90c6e7c12b6a1d1e507b5817158b02f118
HelgaGPataki <herman.carlstrom@gmail.com>
2015-10-09 15:33:30 +0200

    rensat upp kod i toplistfragement

--- BGreen/app/src/main/java/com/bgreen/filips/bgreen/toplist/ToplistFragment.java
+++ BGreen/app/src/main/java/com/bgreen/filips/bgreen/toplist/ToplistFragment.java
@@ -1,14 +1,6 @@
 package com.bgreen.filips.bgreen.toplist;
 
 
-
-import android.app.Application;
-import android.content.DialogInterface;
-
-import android.graphics.drawable.Drawable;
-import android.net.IpPrefix;
-
-
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.SwipeRefreshLayout;
@@ -18,10 +10,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.bgreen.filips.bgreen.R;
-import com.bgreen.filips.bgreen.main.TabActivity;
 import com.bgreen.filips.bgreen.profile.IProfile;
 import com.bgreen.filips.bgreen.profile.ProfileHolder;
 import com.bgreen.filips.bgreen.profile.ProfileService;
@@ -29,7 +19,6 @@ import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 import java.util.List;
-
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
