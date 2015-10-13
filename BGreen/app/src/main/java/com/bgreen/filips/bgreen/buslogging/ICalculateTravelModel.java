89b4f4677b1aa56777bf274bdc01631cfde738c8
Filip Larsson <filip.larsson.vbg@gmail.com>
2015-10-13 11:07:31 +0200

    new CalculateTravel class

---
+++ BGreen/app/src/main/java/com/bgreen/filips/bgreen/buslogging/ICalculateTravelModel.java
@@ -0,0 +1,13 @@
+package com.bgreen.filips.bgreen.buslogging;
+
+import java.util.List;
+
+/**
+ * Created by flarssonn on 2015-10-13.
+ */
+public interface ICalculateTravelModel {
+
+    public void main(String nextStop, String route);
+    public int getFinalResult();
+    public List<String> getTripStops();
+}
