1316b48a5e76c07e9b52b15058afc1f562425be1
sCarlman <carl.albertsson@gmail.com>
2015-10-26 13:51:32 +0100

    Modular Achievements

---
+++ BGreen/app/src/main/java/com/bgreen/filips/bgreen/achievements/IAchievement.java
@@ -0,0 +1,11 @@
+package com.bgreen.filips.bgreen.achievements;
+
+import android.graphics.drawable.Drawable;
+
+/**
+ * Created by Albertsson on 15-10-26.
+ */
+public interface IAchievement {
+
+    String getAchievementImageURL();
+}
