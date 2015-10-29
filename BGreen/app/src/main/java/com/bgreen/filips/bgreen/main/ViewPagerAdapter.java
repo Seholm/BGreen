4a8127f030422dc743caba44ddf742b5b5912a34
sCarlman <carl.albertsson@gmail.com>
2015-10-29 16:00:56 +0100

    ViewPagerAdapter commented

--- BGreen/app/src/main/java/com/bgreen/filips/bgreen/main/ViewPagerAdapter.java
+++ BGreen/app/src/main/java/com/bgreen/filips/bgreen/main/ViewPagerAdapter.java
@@ -1,23 +1,22 @@
 package com.bgreen.filips.bgreen.main;
 
-import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v4.view.ViewPager;
-
-import com.bgreen.filips.bgreen.R;
-
 import java.util.ArrayList;
 import java.util.List;
 
 /**
+ * Inserts the fragments in the TabActivity and returns
+ * a position and the string for the title.
+ *
  * Created by Albertsson on 15-09-15.
  */
 class ViewPagerAdapter extends FragmentPagerAdapter {
 
+    //A list of all the fragments that the adapter inserts in TabActivity
     private final List<Fragment> mFragmentList = new ArrayList<>();
+    //A list of all the titles of the fragments
     private final List<String> mFragmentTitleList = new ArrayList<>();
 
     public ViewPagerAdapter(FragmentManager manager) {
@@ -34,17 +33,14 @@ class ViewPagerAdapter extends FragmentPagerAdapter {
         return mFragmentList.size();
     }
 
+    //Adds the inserted fragment to TabActivity with a title
     public void addFrag(Fragment fragment, String title) {
         mFragmentList.add(fragment);
         mFragmentTitleList.add(title);
     }
 
-
     @Override
     public CharSequence getPageTitle(int position) {
         return mFragmentTitleList.get(position);
     }
-
-
-
 }
